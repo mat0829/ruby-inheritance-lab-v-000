3ceff1fe3597bdcbfa89675c962464cd46c7f467
Mat Anderson <matandersonlv@hotmail.com>
2019-03-14 22:22:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,10 +3,6 @@ require_relative "./user.rb"
 class Student < User
   attr_accessor :knowledge
   
-  def initialize(knowledge)
-    @knowledge = []
-  end
-  
   def new(knowledge)
    @knowledge << knowledge
   
